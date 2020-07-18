c712eeb1b82ce82f9749a36d9124693565a62d1d
gowthamaraj <gowthamaraj360@gmail.com>
2020-07-18 23:25:35 +0530

    basic scrapper designed

--- scrapper.py
+++ scrapper.py
@@ -1,21 +1,14 @@
 from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
-from time import sleep
+from time import sleep,time
 from random import randint
+from warnings import warn
 
 
 pages = [str(i) for i in range(1,5)]
-years_url = [str(i) for i in range(2000,2018)]
+years_url = [str(i) for i in range(2010,2020)]
 
-
-
-url = 'http://www.imdb.com/search/title?release_date=2017&sort=num_votes,desc&page=1'
-response = get(url)
-html_soup = BeautifulSoup(response.text, 'html.parser')
-
-
-movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
 
 names = []
 years = []
@@ -23,22 +16,48 @@ imdb_ratings = []
 metascores = []
 votes = []
 
-for container in movie_containers:
-    if container.find('div', class_ = 'ratings-metascore') is not None:
-        name = container.h3.a.text
-        names.append(name)
+start_time = time()
+requests = 0
+headers = {"Accept-Language": "en-US, en;q=0.5"}
 
-        year = container.h3.find('span', class_ = 'lister-item-year').text
-        years.append(year)
 
-        imdb = float(container.strong.text)
-        imdb_ratings.append(imdb)
+for year_url in years_url:
 
-        m_score = container.find('span', class_ = 'metascore').text
-        metascores.append(int(m_score))
+    for page in pages:
 
-        vote = container.find('span', attrs = {'name':'nv'})['data-value']
-        votes.append(int(vote))
+        response = get('http://www.imdb.com/search/title?release_date=' + year_url +'&sort=num_votes,desc&page=' + page, headers = headers)
+        sleep(randint(8,15))
+        requests += 1
+        elapsed_time = time() - start_time
+        print('Request:{}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
 
-test_df = pd.DataFrame({'movie': names,'year': years,'imdb': imdb_ratings,'metascore': metascores,'votes': votes})
-print(test_df.info())
+        if response.status_code != 200:
+            warn('Request: {}; Status code: {}'.format(requests, response.status_code))
+
+        if requests > 72:
+            warn('Number of requests was greater than expected.')
+            break
+
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+        movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
+
+        for container in movie_containers:
+            if container.find('div', class_ = 'ratings-metascore') is not None:
+                name = container.h3.a.text
+                names.append(name)
+
+                year = container.h3.find('span', class_ = 'lister-item-year').text
+                years.append(year)
+
+                imdb = float(container.strong.text)
+                imdb_ratings.append(imdb)
+
+                m_score = container.find('span', class_ = 'metascore').text
+                metascores.append(int(m_score))
+
+                vote = container.find('span', attrs = {'name':'nv'})['data-value']
+                votes.append(int(vote))
+
+movie_ratings = pd.DataFrame({'movie': names,'year': years,'imdb': imdb_ratings,'metascore': metascores,'votes': votes})
+
+movie_ratings.to_csv('movies.csv')
